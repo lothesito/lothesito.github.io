ecccc29234ebf8d707e7c888c7fff4561034acf6
Lothe <74989561+lothesito@users.noreply.github.com>
2022-09-15 19:56:53 -0500

    v1.0.0
    Subiendo la primera versión

---
+++ script.js
@@ -0,0 +1,89 @@
+var boton_encriptar = document.getElementById("boton-encriptar");
+var boton_desencriptar = document.getElementById("boton-desencriptar");
+var boton_copiar = document.getElementById("boton-copiar");
+var cuadro_texto = document.getElementById("mensaje-no-encontrado")
+var c = 0
+var llave = {
+    a:"ai",
+    e:"enter",
+    i:"imes",
+    o:"ober",
+    u:"ufat"
+
+}
+
+var llaveInversa = {
+    ai:"a",
+    enter:"e",
+    imes:"i",
+    ober:"o",
+    ufat:"u"
+}
+
+function ocultarImagen(){
+    document.getElementById("mensaje-no-encontrado").style.display = "none";
+}
+
+function ocultarTexto(){
+    document.getElementById("texto-capturado").style.display = "none";
+    boton_copiar.style.display = "none"
+}
+
+ocultarTexto()
+
+function mostrarTexto(){
+    document.getElementById("texto-capturado").style.display = "inline"
+    boton_copiar.style.display = "inline"
+}
+
+function animarCuadro(){
+    cuadro_texto 
+}
+
+function encriptar(){
+    let textoCapturado = document.getElementById("texto-de-ingreso").value;
+    if(textoCapturado == ''){
+        alert("Escriba algo por favor")
+    }
+    else{
+        let mensaje = textoCapturado.toLowerCase()
+        ocultarImagen();
+        mostrarTexto();
+        let nuevoMensaje = mensaje.replace(/a|e|i|o|u/g, function(matched){
+            return llave[matched]
+         })
+        document.getElementById("texto-capturado").innerHTML = nuevoMensaje;
+    }
+
+}
+
+function desencriptar(){
+    let textoCapturado = document.getElementById("texto-de-ingreso").value;
+    if(textoCapturado == ''){
+        alert("Escriba algo por favor")
+    }
+    else{
+        let mensaje = textoCapturado.toLowerCase()
+        ocultarImagen();
+        mostrarTexto();
+        let nuevoMensaje = mensaje.replace(/ai|enter|imes|ober|ufat/g, function(matched){
+            return llaveInversa[matched]
+        })
+        document.getElementById("texto-capturado").innerHTML = nuevoMensaje;
+        console.log(nuevoMensaje)
+    }
+}
+
+function copiar(){
+    var contenido = document.getElementById('texto-capturado').innerHTML;
+
+    navigator.clipboard.writeText(contenido)
+ 
+    console.log("Copió");
+
+}
+
+
+boton_desencriptar.onclick = desencriptar;
+boton_encriptar.onclick = encriptar;
+boton_copiar.onclick = copiar;  
